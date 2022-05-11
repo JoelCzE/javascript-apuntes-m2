2234ec922a9dbe1eb07d9e48dc2c20f205dc7bd6
joel <joelce998@gmail.com>
2022-05-11 03:05:44 -0300

    terminado

--- js/reto-11.js
+++ js/reto-11.js
@@ -1,3 +1,3 @@
 let major = Number (prompt("Ingrese un número mayor a 1000"));
 let minor = Number (prompt("Ingrese un número menor a 100"));
-alert ("El numero " + minor + " cabe " + (major/minor - (major%minor)) + " veces en " + major)
+alert ("El numero " + minor + " cabe " + Math.floor((major/minor)) + " veces en " + major)
